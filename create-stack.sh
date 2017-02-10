55da6c5b29db3b9553c239270d96612bad047a4d
Derek Yeager <dcy2003@gmail.com>
2017-02-10 17:27:49 -0500

    bkup

--- create-stack.sh
+++ create-stack.sh
@@ -2,9 +2,6 @@
 
 aws cloudformation create-stack \
     --stack-name DCOS \
-    --template-body file://infrastructure-prerequisites.json
- #   --parameters  \
- #   ParameterKey=EC2SshKey,ParameterValue=cfn-key-pair \
- #   ParameterKey=S3LocationOfPrivateKey,ParameterValue=s3://asc-cloudformation-resources/cfn-key-pair.pem \
- #   ParameterKey=GitHubRepository,ParameterValue=git@github.com:dcy2003/cloud-formation-example.git \
- #   ParameterKey=AnsiblePlaybook,ParameterValue=ansible-playbook.yaml
+    --template-body file://infrastructure-prerequisites.json \
+    --parameters  \
+    ParameterKey=KeyName,ParameterValue=dcos-west-key-pair
