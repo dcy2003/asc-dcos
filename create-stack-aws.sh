952929604729ce58f77a11094fcc9e8865436aa3
Derek Yeager <dcy2003@gmail.com>
2017-02-11 20:19:46 -0500

    in progress

--- create-stack-aws.sh
+++ create-stack-aws.sh
@@ -4,4 +4,5 @@ aws cloudformation create-stack \
     --stack-name DCOS \
     --template-body file://infrastructure-prerequisites.json \
     --parameters  \
+    ParameterKey=Environment,ParameterValue=aws \
     ParameterKey=KeyName,ParameterValue=dcos-west-key-pair
