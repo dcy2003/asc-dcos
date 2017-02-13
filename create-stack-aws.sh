450017dcae43c929b4bfe16dacc3a8edb25f9412
Derek Yeager <dcy2003@gmail.com>
2017-02-13 12:43:13 -0500

    Done everything but private subnet and S3 bucket

--- create-stack-aws.sh
+++ create-stack-aws.sh
@@ -2,7 +2,13 @@
 
 aws cloudformation create-stack \
     --stack-name DCOS \
-    --template-body file://infrastructure.json \
+    --template-body file://ZEN.json \
     --parameters  \
     ParameterKey=Environment,ParameterValue=aws \
+    ParameterKey=PublicAgentRole,ParameterValue=arn:aws:iam::237001706895:role/DCOS-EE-Integration-Environment-3-PublicAgentRole-CN5U3WTVSIL1 \
+    ParameterKey=PublicAgentInstanceProfile,ParameterValue=arn:aws:iam::237001706895:instance-profile/DCOS-EE-Integration-Environment-3-Master-PublicAgentStack-WLLD3ZQ5DN5W-PublicAgentInstanceProfile-1NGHMYX52I3KY \
+    ParameterKey=PrivateAgentRole,ParameterValue=arn:aws:iam::237001706895:role/DCOS-EE-Integration-Environment-3-PrivateAgentRole-7LGADWM3WN9O \
+    ParameterKey=PrivateAgentInstanceProfile,ParameterValue=arn:aws:iam::237001706895:instance-profile/DCOS-EE-Integration-Environment-3-Master-PrivateAgentStack-10IGL5ZPR7H4F-PrivateAgentInstanceProfile-19510YIWJOX1J \
+    ParameterKey=MasterRole,ParameterValue=arn:aws:iam::237001706895:role/DCOS-EE-Integration-Environment-3-Maste-MasterRole-GO2795NLF3EE \
+    ParameterKey=MasterInstanceProfile,ParameterValue=arn:aws:iam::237001706895:instance-profile/DCOS-EE-Integration-Environment-3-Master-MasterStack-CWJ1A4FJ4V0R-MasterInstanceProfile-DXY306E5R2CC \
     ParameterKey=KeyName,ParameterValue=dcos-west-key-pair
--- create-stack-c2s.sh
+++ create-stack-c2s.sh
@@ -2,9 +2,15 @@
 
 aws cloudformation create-stack \
     --stack-name DCOS \
-    --template-body file://infrastructure.json \
+    --template-body file://ZEN.json \
     --parameters  \
     ParameterKey=Environment,ParameterValue=c2s \
     ParameterKey=VpcId,ParameterValue=FILL_ME_IN \
     ParameterKey=PublicSubnetId,ParameterValue=FILL_ME_IN \
+    ParameterKey=PublicAgentRole,ParameterValue=FILL_ME_IN \
+    ParameterKey=PublicAgentInstanceProfile,ParameterValue=FILL_ME_IN \
+    ParameterKey=PrivateAgentRole,ParameterValue=FILL_ME_IN \
+    ParameterKey=PrivateAgentInstanceProfile,ParameterValue=FILL_ME_IN \
+    ParameterKey=MasterRole,ParameterValue=FILL_ME_IN \
+    ParameterKey=MasterInstanceProfile,ParameterValue=FILL_ME_IN \
     ParameterKey=KeyName,ParameterValue=dcos-west-key-pair
