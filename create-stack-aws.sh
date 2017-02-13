b90d76c28adbe9daa4bff3c74cfc4bb45970d9a7
Derek Yeager <dcy2003@gmail.com>
2017-02-13 16:27:39 -0500

    Modified s3 bucket param again

--- create-stack-aws.sh
+++ create-stack-aws.sh
@@ -12,5 +12,5 @@ aws cloudformation create-stack \
     ParameterKey=MasterRole,ParameterValue=arn:aws:iam::237001706895:role/DCOS-EE-Integration-Environment-3-Maste-MasterRole-GO2795NLF3EE \
     ParameterKey=MasterInstanceProfile,ParameterValue=arn:aws:iam::237001706895:instance-profile/DCOS-EE-Integration-Environment-3-Master-MasterStack-CWJ1A4FJ4V0R-MasterInstanceProfile-DXY306E5R2CC \
     ParameterKey=KeyName,ParameterValue=dcos-west-key-pair \
-    ParameterKey=ExhibitorS3Bucket,ParameterValue=https://s3.amazonaws.com/dcos-east-ha-exhibitor \
+    ParameterKey=ExhibitorS3Bucket,ParameterValue=dcos-east-ha-exhibitor \
     --capabilities CAPABILITY_NAMED_IAM
